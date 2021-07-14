bc39fe2f0c03f5b9f3523d1ed18eb7c4470c0814
bvpav <bvpav@tutanota.com>
2021-07-14 18:43:41 +0300

    Added initial Note model

--- website/notes/models.py
+++ website/notes/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Just a friendly reminder to makemigrations and migrate after changing this file :)
+
+
+class Note(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(_('Name of Note'), max_length=64)
+
+    def __str__(self):
+        return self.title
