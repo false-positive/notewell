cb7fb6d756e80efa8d79c6e022d9e945eebf8c20
bvpav <bvpav@tutanota.com>
2021-08-17 17:43:30 +0300

    Added editor header

---
+++ editor/src/components/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let title = '';
+</script>
+
+<header>
+    <div class="container">
+        <nav>
+            <a href="/notes/">
+                <!--
+                    so the logo is taken from freepik and they
+                    insist that I attribute them, but
+                    this is a placehoder, sooo...
+                -->
+                <img src="/static/notes/img/logo.png" alt="Logo" class="logo" />
+            </a>
+            <p contenteditable={true}>{title}</p>
+        </nav>
+    </div>
+</header>
+
+<style>
+    p {
+        font-size: 2rem;
+        padding-left: 0.4rem;
+        font-weight: 300;
+        color: #fff;
+    }
+</style>
