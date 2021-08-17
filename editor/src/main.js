af17e4e98fc7fdf1ebe123e6a64bdb5025104fc9
bvpav <bvpav@tutanota.com>
2021-08-17 16:26:21 +0300

    Made editor work with the base template

--- editor/src/main.js
+++ editor/src/main.js
@@ -1,7 +1,10 @@
-import App from './App.svelte'
+import App from './App.svelte';
+
+// HACK: clean the contents of #app
+document.getElementById('app').innerHTML = '';
 
 const app = new App({
-  target: document.getElementById('app')
-})
+    target: document.getElementById('app'),
+});
 
-export default app
+export default app;
