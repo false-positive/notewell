bf5d73125a0b1b5f6bdcb2140d26015f330ebce6
bvpav <bvpav@tutanota.com>
2021-08-17 17:59:22 +0300

    Fixed api endpoint urls

--- website/api/urls.py
+++ website/api/urls.py
@@ -5,19 +5,18 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('users/', views.view_users, name='user-list'),
-    #     path('user/', views.view_notes, name='note-list-all'),
 
     path('login/', obtain_auth_token, name='login'),
     path('register/', views.register, name='register'),
 
     path('notes/', views.view_notes, name='note-list-all'),
-    path('notes/<path:cat_path>/', views.view_notes, name='note-list-detail'),
-    path('note/', views.note_crud, name='note-create'),
-    path('note/<uuid:note_id>/', views.note_crud, name='note-detail'),
-    path('note/<uuid:note_id>/', views.note_crud, name='note-update'),
-    path('note/<uuid:note_id>/', views.note_crud, name='note-delete'),
+    path('notes/', views.note_crud, name='note-create'),
+    path('notes/<uuid:note_id>/', views.note_crud, name='note-detail'),
+    path('notes/<uuid:note_id>/', views.note_crud, name='note-update'),
+    path('notes/<uuid:note_id>/', views.note_crud, name='note-delete'),
 
     path('categories/', views.view_categories, name='category-list-all'),
     path('categories/<path:cat_path>/',
-         views.view_categories, name='category-detail')
+         views.view_categories, name='category-detail'),
+    path('notes/<path:cat_path>/notes/', views.view_notes, name='note-list-detail'),
 ]
