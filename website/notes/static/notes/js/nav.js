e58a054b602c3743fca345ed463c05d25b041c27
bvpav <bvpav@tutanota.com>
2021-08-23 23:55:52 +0300

    Fixed this again
    Why didnt it get saved what did I not do last time

--- website/notes/static/notes/js/nav.js
+++ website/notes/static/notes/js/nav.js
@@ -2,12 +2,7 @@
 const selectElement = (element) => document.querySelector(element);
 
 //Open and close nav on click
-<<<<<<< HEAD
 // XXX: some browsers don't support `?.`
-selectElement('.menu-icons')?.addEventListener('click', () => {
-    selectElement('nav').classList.toggle('active');
-=======
-selectElement('.nav__hamburger').addEventListener('click', () => {
+selectElement('.nav__hamburger')?.addEventListener('click', () => {
     selectElement('.nav').classList.toggle('active');
->>>>>>> development
 });
