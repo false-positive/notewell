f0c2541eaedfe7895518ec47d14da31835121346
bvpav <bvpav@tutanota.com>
2021-08-23 15:51:55 +0300

    Refactored header styles to follow BEM

--- website/notes/static/notes/js/nav.js
+++ website/notes/static/notes/js/nav.js
@@ -2,6 +2,6 @@
 const selectElement = (element) => document.querySelector(element);
 
 //Open and close nav on click
-selectElement('.menu-icons').addEventListener('click', () => {
-    selectElement('nav').classList.toggle('active');
+selectElement('.nav__hamburger').addEventListener('click', () => {
+    selectElement('.nav').classList.toggle('active');
 });
