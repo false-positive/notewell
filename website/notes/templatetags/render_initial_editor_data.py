3311c62104e9078ed0b9708ba8f24cc8523cd098
bvpav <bvpav@tutanota.com>
2021-08-24 17:02:13 +0300

    Added tag for embedding editor bootstrapping data

---
+++ website/notes/templatetags/render_initial_editor_data.py
@@ -0,0 +1,27 @@
+"""Render input data for editor.
+
+Creates a <script> tag with type="application/json" and
+id="nw-editor-initial-data".
+"""
+
+import json
+
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.simple_tag
+def render_initial_editor_data(*, note, token_pair):
+    # XXX: may be unsafe ?
+    return mark_safe(json.dumps({
+        # 'note': {
+        #     'author': note.author.username,
+        #     'uuid': note.uuid,
+        #     'title': note.title,
+        #     'date_created': note.date_created,
+        #     'categories': []
+        # },
+        'definitely_not_token_pair': token_pair,
+    }, separators=(',', ':')))
