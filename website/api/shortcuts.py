ed467abf0a4e1bd956643c0d0ab472ddf3b01bad
bvpav <bvpav@tutanota.com>
2021-08-24 15:59:44 +0300

    Refactorede jwt generation shorcut

--- website/api/shortcuts.py
+++ website/api/shortcuts.py
@@ -1,8 +1,9 @@
-from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 def generate_jwt_token(user):
+    refresh = RefreshToken.for_user(user)
     return {
-        "access": str(AccessToken.for_user(user)),
-        "refresh": str(RefreshToken.for_user(user))
+        'access': str(refresh.access_token),
+        'refresh': str(refresh),
     }
