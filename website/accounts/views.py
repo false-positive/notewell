ddf819214e30e3769ff70becf62d3ff3d950885c
Kerzoken <kerzo.gm@gmail.com>
2021-09-10 11:59:13 +0300

    Designed login and register forms with material design

--- website/accounts/views.py
+++ website/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .forms import UserRegisterForm
+from .forms import UserLoginForm, UserRegisterForm
 
 
 def register(request):
@@ -10,6 +10,12 @@ def register(request):
             form.save()
             # TODO: Maybe add `Success` message here
             return redirect('accounts:login')
+        else:
+            for field, errors in form.errors.as_data().items():
+                # print(f'{field}: {errors[0]}')
+                form.fields[field].widget.attrs['data-nw-error'] = errors[0].message
+
     else:
         form = UserRegisterForm()
+
     return render(request, 'accounts/register.html', {'form': form})
