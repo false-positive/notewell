3c58280ac4e904082383cf7ae7dc045b1cb4336b
CodenameSource <kaloyanmiladinov@gmail.com>
2021-10-27 13:43:55 +0300

    remove unnecessary files

--- Questgen/main.py
+++ Questgen/main.py
@@ -1,22 +1,27 @@
 from flask import Flask, request
 import Questgen
 
-from answer_processing.clean_answer import clean_answer
-
 app = Flask(__name__)
-nlp = Questgen.main.QGen()
+nlp = Questgen.main
 
 
 @app.route('/generate_question', methods=('POST',))
 def generate_question():
     input_ = request.json
-    output = nlp.predict_mcq(input_)
+    if input_['type'] == 'MCQ':
+        qe = nlp.QGen()
+        output = qe.predict_mcq({'input_text': input_['input_text']})
+        output = output['questions']
+    elif input_['type'] == 'Bool':
+        qe =nlp.BoolQGen()
+        output = qe.predict_boolq({'input_text': input_['input_text']})
+        output = output['Boolean Questions']
     if len(output) == 0:
         return {
             'output': 'Something happened and the algorithm failed to return anything, most likely the text is too short'
         }, 500
     return {
-        'output': output['questions']
+        'output': output
     }
 
 
