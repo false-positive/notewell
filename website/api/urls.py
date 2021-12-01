01dd742a211ad8e9e3a559a794d96ffc54b1cc51
bvpav <bvpav@tutanota.com>
2021-12-01 19:02:59 +0200

    Fixed errors in URLconfs
    Apparently somebody (that shall not be named) edited URLconfs without
    ever running the server and encountering the two page loud error
    message that prevents the whole thing to run. Oh well.. happens to the
    best of us

--- website/api/urls.py
+++ website/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from . import views
-
-# from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenRefreshView
+
+from . import views
 
 urlpatterns = [
     path('token/', views.UserTokenPairView.as_view(), name='token_obtain_pair'),
@@ -15,10 +14,10 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
 
-    path('notes/summarize/', views.summarize(), name='summarize'),
-    path('notes/questgen/', views.genquest(), name='questgen'),
-    path('notes/quality/', views.quality(), name='quality'),
-    path('notes/subject/', views.subject(), name='subject'),
+    path('notes/summarize/', views.summarize, name='summarize'),
+    path('notes/questgen/', views.genquest, name='questgen'),
+    path('notes/quality/', views.quality, name='quality'),
+    path('notes/subject/', views.subject, name='subject'),
 
     path('notes/', views.NoteList.as_view(), name='note_list'),
     path('notes/<uuid:note_id>/', views.NoteDetail.as_view(), name='note_detail'),
