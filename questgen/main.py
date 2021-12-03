fbe1cc4665208eaed2f9bdb0ccc9acf0feaa0231
bvpav <bvpav@tutanota.com>
2021-12-03 23:38:39 +0200

    Refactored Questgen server
    Please, just P L E A S E, use `snake_case`

--- questgen/main.py
+++ questgen/main.py
@@ -1,26 +1,25 @@
 import os
-import Questgen
 from flask import Flask, request
+from Questgen import main as nlp
 
-
-#os.environ['TRANSFORMERS_CACHE'] = 'D:\huggingface\cache' made this so huggingface transformers model in questgen could work on my pc with limited C: space left
 app = Flask(__name__)
-nlp = Questgen.main
-boolQG = nlp.BoolQGen()
-MQG = nlp.QGen()
+bool_qgen = nlp.BoolQGen()
+multi_qgen = nlp.QGen()
+
 
 @app.route('/generate_question', methods=('POST',))
 def generate_question():
     input_ = request.json
-    print(input_)
-    output = ""
+    output = None
+
     if input_['type'] == 'MCQ':
-        output = MQG.predict_mcq({'input_text': input_['input_text']})
+        output = multi_qgen.predict_mcq({'input_text': input_['input_text']})
         output = output['questions']
     elif input_['type'] == 'Bool':
-        output = boolQG.predict_boolq({'input_text': input_['input_text']})
+        output = bool_qgen.predict_boolq({'input_text': input_['input_text']})
         output['Questions'] = output['Boolean Questions']
-    if len(output) == 0:
+
+    if not output:
         return {
             'output': 'Something happened and the algorithm failed to return anything, most likely the text is too short'
         }, 500
