440be200f267b767acdeea9f3c1ac16f3833c1f9
bvpav <bvpav@tutanota.com>
2022-02-19 16:04:58 +0200

    Added initial data to context

--- editor/src/main.tsx
+++ editor/src/main.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { InitialDataContext, loadInitialData } from './api/initialData';
 
 // HACK: clean the contents of #app
 const root = document.getElementById('app')!;
 root.innerHTML = '';
 
-// const initialDataEl = document.getElementById('nw-editor-initial-data')!;
-// const initialData = JSON.parse(initialDataEl.innerHTML);
-// initialDataEl.outerHTML = '';
+const initialData = loadInitialData();
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <InitialDataContext.Provider value={initialData}>
+            <App />
+        </InitialDataContext.Provider>
     </React.StrictMode>,
     root
 );
