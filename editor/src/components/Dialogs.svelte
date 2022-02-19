c05c3a42dd845cd8f7f83344362022032ff86a9c
bvpav <bvpav@tutanota.com>
2022-02-19 12:15:08 +0200

    Deleted svelte source
    I know this is hard to do, but we have to trust the process
    
    😪😭😢⚱

--- editor/src/components/Dialogs.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import ShareDialog from './dialogs/ShareDialog.svelte';
-    import { isShareDialogOpen } from '../stores/isShareDialogOpen';
-</script>
-
-<ShareDialog bind:open={$isShareDialogOpen} />
