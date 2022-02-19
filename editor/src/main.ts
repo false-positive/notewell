8616f73d4d53ac6d428d75de53553ff9ff6a86c6
bvpav <bvpav@tutanota.com>
2022-02-19 13:50:02 +0200

    Moved tsconfig.json and fixed errors
    Now it builds, yay

--- editor/src/main.ts
+++ editor/src/main.ts
@@ -1,12 +1,14 @@
 // HACK: clean the contents of #app
-document.getElementById('app').innerHTML = '';
+document.getElementById('app')!.innerHTML = '';
 
-const initialDataEl = document.getElementById('nw-editor-initial-data');
+const initialDataEl = document.getElementById('nw-editor-initial-data')!;
 const initialData = JSON.parse(initialDataEl.innerHTML);
 initialDataEl.outerHTML = '';
 
-document.getElementById('app').innerHTML = `<pre>${JSON.stringify(
+document.getElementById('app')!.innerHTML = `<pre>${JSON.stringify(
     initialData,
     null,
     4
 )}</pre>`;
+
+export {};
