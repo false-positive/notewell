8543671b56ea911bd996eae5b9fdb30a94633592
bvpav <bvpav@tutanota.com>
2022-02-21 10:15:03 +0200

    Added note categories button

--- editor/src/components/EditorHeader/NoteTitleCategories.tsx
+++ editor/src/components/EditorHeader/NoteTitleCategories.tsx
@@ -1,7 +1,8 @@
-import { Box, ButtonBase, Skeleton, Stack, Typography } from '@mui/material';
+import { IconButton, Skeleton, Stack } from '@mui/material';
 import { FC } from 'react';
 import { Note } from '../../api/notes';
 import NoteTitle from './NoteTitle';
+import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
 
 type Props = {
     note?: Note;
@@ -10,12 +11,17 @@ type Props = {
 const NoteTitleCategories: FC<Props> = ({ note }) => {
     return (
         <Stack direction="row" spacing={2} alignItems="center">
-            {note ? (
-                <NoteTitle title={note.title} />
-            ) : (
+            {!note ? (
                 <Skeleton animation="wave">
                     <NoteTitle title="jdfghujsgfhoiwueroighuobjsdfklg" />
                 </Skeleton>
+            ) : (
+                <>
+                    <NoteTitle title={note.title} />
+                    <IconButton>
+                        <LibraryBooksOutlinedIcon />
+                    </IconButton>
+                </>
             )}
         </Stack>
     );
