90bc41b1fbf13b5a513d9c9756384aaaff78532a
bvpav <bvpav@tutanota.com>
2022-02-23 23:17:09 +0200

    Removed leftover vendored dependency

--- vendor/quill-delta-markdown/index.js
+++
@@ -1,2 +0,0 @@
-export { default as fromDelta } from './src/fromDelta';
-export { default as toDelta } from './src/toDelta';
--- vendor/quill-delta-markdown/src/utils/URL.js
+++
@@ -1,5 +0,0 @@
-export const encodeLink = (link) =>
-  encodeURI(link)
-    .replace(/\(/i, "%28")
-    .replace(/\)/i, "%29")
-    .replace(/(\?|\&)response-content-disposition=attachment.*$/, "")
