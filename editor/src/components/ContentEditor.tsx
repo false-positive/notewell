e0e8d34f26dd8115181d0c7276f40059aab9bb3f
bvpav <bvpav@tutanota.com>
2022-02-23 11:24:27 +0200

    Updated it to use react-quilljs

--- editor/src/components/ContentEditor.tsx
+++ editor/src/components/ContentEditor.tsx
@@ -1,25 +1,10 @@
-import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
-import { useCallback, useState } from 'react';
+import { useQuill } from 'react-quilljs';
 
 const ContentEditor = () => {
-    const [quill, setQuill] = useState<Quill | null>(null);
+    const { quill, quillRef } = useQuill();
 
-    const wrapperRef = useCallback((wrapper) => {
-        if (wrapper == null) return;
-
-        wrapper.innerHTML = '';
-        const editor = document.createElement('div');
-        wrapper.append(editor);
-        const q = new Quill(editor, {
-            theme: 'snow',
-            // modules: { toolbar: TOOLBAR_OPTIONS },
-        });
-        // q.disable();
-        // q.setText('Loading...');
-        setQuill(q);
-    }, []);
-    return <div ref={wrapperRef} />;
+    return <div ref={quillRef} />;
 };
 
 export default ContentEditor;
