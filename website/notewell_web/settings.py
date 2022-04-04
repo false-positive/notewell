65b242b18505079df2ee78f618e116feedfab8e3
bvpav <bvpav@tutanota.com>
2022-04-04 22:52:24 +0300

    Added session authentication classes

--- website/notewell_web/settings.py
+++ website/notewell_web/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
