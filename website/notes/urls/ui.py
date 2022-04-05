2743dad5306cb187f9f49ab0e796b37a4a77e671
bvpav <bvpav@tutanota.com>
2022-04-05 16:57:06 +0300

    Made all notes routes view the react frontend

--- website/notes/urls/ui.py
+++ website/notes/urls/ui.py
@@ -1,17 +1,20 @@
 from django.urls import path
+from django.views import generic
 
 from notes.views import ui as views
 
 
 app_name = 'notes'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('my/', views.my, name='my'),
-    path('new/', views.NoteCreateView.as_view(), name='create'),
-    path('<uuid:note_id>/', views.read, name='read'),
-    path('<uuid:note_id>/edit/', views.editor, name='edit'),
-    path('<uuid:note_id>/quiz/', views.editor, name='quiz'),
-    path('<uuid:note_id>/delete/', views.NoteDeleteView.as_view(), name='delete'),
-    path('<uuid:note_id>/publish/', views.publish, name='publish'),
-    path('<path:cat_path>/', views.index, name='category'),
+    # path('', views.index, name='index'),
+    # path('my/', views.my, name='my'),
+    # path('new/', views.NoteCreateView.as_view(), name='create'),
+    # path('<uuid:note_id>/', views.read, name='read'),
+    # path('<uuid:note_id>/edit/', views.editor, name='edit'),
+    # path('<uuid:note_id>/quiz/', views.editor, name='quiz'),
+    # path('<uuid:note_id>/delete/', views.NoteDeleteView.as_view(), name='delete'),
+    # path('<uuid:note_id>/publish/', views.publish, name='publish'),
+    # path('<path:cat_path>/', views.index, name='category'),
+    path('', generic.TemplateView.as_view(template_name='notes/edit.html'), name='index'),
+    path('<path:route>/', generic.TemplateView.as_view(template_name='notes/edit.html')),
 ]
